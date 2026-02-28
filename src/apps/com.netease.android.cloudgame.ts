fe1d2d68fd31b8dcbca6ed1abec98519779b491a
cjy0812 <cjy081219@Gmail.com>
2026-02-28 17:38:44 +0800

    perf: 网易云游戏 局部广告_规则细分

--- src/apps/com.netease.android.cloudgame.ts
+++ src/apps/com.netease.android.cloudgame.ts
@@ -20,26 +20,45 @@ export default defineGkdApp({
     {
       key: 2,
       name: '局部广告',
+      fastQuery: true,
+      activityIds: '.activity.MainActivity',
       rules: [
         {
           key: 0,
-          fastQuery: true,
-          activityIds: '.activity.MainActivity',
-          anyMatches: [
-            '@ImageView[clickable=true][visibleToUser=true] -n RelativeLayout > [text*="广告"]',
+          name: '底部横幅广告',
+          anyMatches:
             'ViewGroup[childCount=2] > [vid="banner_content"] + [vid="close_btn"][clickable=true][visibleToUser=false]',
+          snapshotUrls: 'https://i.gkd.li/i/25573674',
+          exampleUrls: 'https://e.gkd.li/bfe2d2a2-2199-425c-9729-9176aa4fb5bf',
+        },
+        {
+          key: 1,
+          name: '福利页广告',
+          anyMatches: [
             '@ImageView[clickable=true][visibleToUser=true] -2 ImageView < RelativeLayout[childCount=3] <<n [vid="sign_ad_card"]',
             'ImageView < * -2 * >n @ImageView[visibleToUser=true] < * -2 * < FrameLayout[childCount=3] <<n [vid="sign_ad_card"]',
-            'ImageView -3 @ImageView[clickable=true][visibleToUser=true] - * < RelativeLayout[childCount=5] <<n [vid="mine_ui_ad_layout"]',
           ],
           snapshotUrls: [
-            'https://i.gkd.li/i/25573743',
-            'https://i.gkd.li/i/25573674',
             'https://i.gkd.li/i/25574104',
             'https://i.gkd.li/i/25619258',
           ],
           exampleUrls: [
             'https://e.gkd.li/91afc489-ac6e-452c-9b75-d15336e11989',
+            'https://e.gkd.li/217312d8-5dd5-48c3-a6c2-448608674957',
+          ],
+        },
+        {
+          key: 2,
+          name: '个人页广告',
+          anyMatches: [
+            'ImageView -3 @ImageView[clickable=true][visibleToUser=true] - * < RelativeLayout[childCount=5] <<n [vid="mine_ui_ad_layout"]',
+            '@ImageView[clickable=true][visibleToUser=true] -n RelativeLayout > [text*="广告"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/25573743',
+            'https://i.gkd.li/i/25620233',
+          ],
+          exampleUrls: [
             'https://e.gkd.li/217312d8-5dd5-48c3-a6c2-448608674957',
             'https://e.gkd.li/b1194340-db45-4ece-85a5-04447b12a2e9',
           ],
