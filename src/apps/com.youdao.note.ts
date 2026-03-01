dfee40fa1d80827d0b3603b1a661b059b52fdaf3
longmo <3131521901@qq.com>
2026-03-01 17:15:52 +0800

    feat: Add Youdao Note tasks and update package name

--- src/apps/com.youdao.note.ts
+++ src/apps/com.youdao.note.ts
@@ -19,5 +19,31 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 10,
+      name: '签到任务',
+      desc: '自动点击包含【签到】文字的按钮',
+      rules: [
+        {
+          matches: [
+            "Button[id='com.youdao.note:id/sign_in' && text='签到得空间']",
+          ],
+          fastQuery: true,
+          snapshotUrls: ['https://i.gkd.li/i/your_snapshot_url'],
+          activityIds: ['com.youdao.note.activity2.MainActivity'],
+        },
+      ],
+    },
+    {
+      key: 20,
+      name: '有道云笔记-领取更多空间(文本)',
+      actionMaximum: 1,
+      rules: [
+        {
+          matches: ["TextView[text='领取更多空间']"],
+          activityIds: ['com.youdao.note.activity2.TaskListActivity'],
+        },
+      ],
+    },
   ],
 });
